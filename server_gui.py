b55bd8649e28debd4f58a503d2098a15b99e307d
joncoop <joncoop1972@gmail.com>
2017-05-01 17:28:22 -0400

    Are you down with OOP?

--- server_gui.py
+++ server_gui.py
@@ -1,59 +1,78 @@
 #!/usr/bin/python3
 
 import socket
-#import time
+import time
 import threading
 from tkinter import *
 
-# Hacky helper function
-def get_local_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ip = s.getsockname()[0]
-    s.close()
+
+class Server:
     
-    return ip
+    def __init__(self):
+        self.running = False
+        
+    # Hacky helper function
+    def get_local_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        s.close()
+        
+        return ip
 
-# Do the network/socket stuff
-def process_messages():
-    '''
-    print('processing messages')
-    while True:
-        if running:
-            print("running")
-            time.sleep(1)
-    '''
-    host = get_local_ip()
-    port = 5000
+    # Server controls
+    def run(self):
+        thread = threading.Thread(target=self.process_messages)
+        thread.start()
 
-    clients = []
+    def start(self):
+        self.running = True
+        
+    def stop(self):
+        self.running = False
+        
+    # Do the network/socket stuff
+    def process_messages(self):
+        
+        '''
+        print('processing messages')
+        while True:
+            if running:
+                print("running")
+                time.sleep(1)
+        '''
+        host = self.get_local_ip()
+        port = 5000
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind((host, port))
-    s.setblocking(0)
+        clients = []
 
-    while True:
-        if running:
-            try:
-                data, addr = s.recvfrom(1024)
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind((host, port))
+        s.setblocking(0)
 
-                if addr not in clients:
-                    clients.append(addr)
-                    
-                print(time.ctime(time.time()) + str(addr) + ": :" + str(data))
-                for client in clients:
-                    s.sendto(data, client)
-            except:
-                pass
+        while True:
+            if self.running:
+                try:
+                    data, addr = s.recvfrom(1024)
 
+                    if addr not in clients:
+                        clients.append(addr)
+                        
+                    print(time.ctime(time.time()) + str(addr) + ": :" + str(data))
+                    for client in clients:
+                        s.sendto(data, client)
+                except:
+                    pass
+        
 # A nifty GUI
 class App:
     
-    def __init__(self, master):
+    def __init__(self, master, server):
         self.master = master
+        self.server = server
         
         # Network stuff
-        self.host_ip = get_local_ip()
+        self.host_ip = server.get_local_ip()
         self.port = 5000
 
         # Server status
@@ -95,29 +114,24 @@ class App:
         self.stop_btn.pack(side=RIGHT)
 
         
-    def start(self):
-        global running
-        
-        running = True
+    def start(self):        
+        self.server.start()
         app.state.set("Server is running at")
         
-    def stop(self):
-        global running
-        
-        running = False
+    def stop(self):        
+        self.server.stop()
         app.state.set("Server is stopped at")
 
 
 if __name__ == "__main__":
     running = False
-    thread = threading.Thread(target=process_messages)
-    thread.start()
-
+    
+    server = Server()
+    server.run()
+    
     root = Tk()
-    app = App(root)
+    app = App(root, server)
     root.mainloop()
-
-    thread.join()
 
 
 # http://stackoverflow.com/questions/26703502/threads-and-tkinter-python-3
